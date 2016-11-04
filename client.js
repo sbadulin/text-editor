cd232e7fdd1a9c675efb7675276ab9750f504a3f
Sergey Badulin <sbadulin@gmail.com>
2016-11-04 21:04:43 +0300

    add tags - detection, listing, filtration, deletion

--- client.js
+++ client.js
@@ -1,12 +1,16 @@
 var noteList = {
   notes: [],
-  addNote: function(noteText) {
+  addNote: function(noteText, tags) {
     this.notes.push({
-      noteText: noteText
+      noteText: noteText,
+      tags: tags
     });
   },
-  changeNote: function(position, noteText) {
-    this.notes[position].noteText = noteText;
+  changeNote: function(position, noteText, tags) {
+    var changedNote = this.notes[position];
+    changedNote.noteText = noteText;
+    changedNote.tags = [];
+    this.notes[position].tags = tags;
   },
   deleteNote: function(position) {
     this.notes.splice(position, 1);
@@ -17,16 +21,35 @@ var noteList = {
   }
 };
 
+var tagList = {
+  tags: [],
+  addTags: function(noteTags) {
+    noteTags.forEach(function(tag) {
+      if (!tagList.tags.includes(tag)) {
+        tagList.tags.push(tag);
+      }
+    });
+  },
+  deleteTag: function(tag) {
+    var index = this.tags.indexOf(tag);
+    this.tags.splice(index, 1);
+  }
+};
+
 var handlers = {
   addNote: function() {
     var addNoteTextInput = document.getElementById('addNoteTextInput');
-    noteList.addNote(addNoteTextInput.value);
+    var tags = this.findNoteTags();
+    noteList.addNote(addNoteTextInput.value, tags);
+    this.displayTagList();
     addNoteTextInput.value = '';
     view.displayAllNotes();
   },
   changeNote: function(position) {
     var addNoteTextInput = document.getElementById('addNoteTextInput');
-    noteList.changeNote(position, addNoteTextInput.value);
+    var tags = this.findNoteTags();
+    noteList.changeNote(position, addNoteTextInput.value, tags);
+    this.displayTagList();
     addNoteTextInput.value = '';
     view.displayAllNotes();
   },
@@ -39,12 +62,29 @@ var handlers = {
     saveButton.onclick = function() {
       handlers.changeNote(position);
     };
+  },
+  findNoteTags: function() {
+    var noteTags = addNoteTextInput.value.match(/\#[a-zA-zа-яА-я0-9]+/g);
+    if (noteTags) {
+      tagList.addTags(noteTags);
+    }
+    return noteTags;
+  },
+  displayTagList: function() {
+    view.displayTagList();
+  },
+  deleteTag: function(tag) {
+    tagList.deleteTag(tag);
+    this.displayTagList();
+  },
+  displayNotesWithTag: function(tag) {
+    view.displayNotesWithTag(tag);
   }
 };
 
 var view = {
   displayAllNotes: function() {
-    var notesUl = document.querySelector('ul');
+    var notesUl = document.querySelector('ul#notes');
     notesUl.innerHTML = '';
     noteList.notes.forEach(function(note, position) {
       var noteLi = document.createElement('li');
@@ -57,9 +97,23 @@ var view = {
       handlers.addNote();
     };
   },
+  displayNotesWithTag: function(tag) {
+    var notesUl = document.querySelector('ul#notes');
+    notesUl.innerHTML = '';
+    noteList.notes.forEach(function(note, position) {
+      var noteLi = document.createElement('li');
+      noteLi.textContent = note.noteText;
+      noteLi.id = position;
+      if (note.tags && note.tags.includes(tag)) {
+        notesUl.appendChild(noteLi);
+        noteLi.appendChild(this.createDeleteButton());
+      }
+    }, this);
+    saveButton.onclick = function() {
+      handlers.addNote();
+    };
+  },
   displayNote: function(position) {
-    // var selectedNote = document.getElementById(position);
-    // selectedNote.className = 'selected';
     var textarea = document.getElementById('addNoteTextInput');
     textarea.value = noteList.displayNote(position);
   },
@@ -69,8 +123,27 @@ var view = {
     deleteButton.className = 'deleteButton';
     return deleteButton;
   },
+  displayTagList: function() {
+    var tagsUl = document.querySelector('ul#tags');
+    tagsUl.innerHTML = '';
+    if (tagList.tags) {
+      tagList.tags.forEach(function(tag) {
+        var tagLi = document.createElement('li');
+        tagLi.textContent = tag.slice(1);
+        tagLi.dataset.tag = tag;
+        tagsUl.appendChild(tagLi);
+        tagLi.appendChild(this.createTagDeleteButton());
+      }, this);
+    }
+  },
+  createTagDeleteButton: function() {
+    var tagDeleteButton = document.createElement('button');
+    tagDeleteButton.textContent = 'x';
+    tagDeleteButton.className = 'tagDeleteButton';
+    return tagDeleteButton;
+  },
   setUpEventListeners: function() {
-    var notesUl = document.querySelector('ul');
+    var notesUl = document.querySelector('ul#notes');
     notesUl.addEventListener('click', function(event) {
       var elementClicked = event.target;
       if (elementClicked.className === 'deleteButton') {
@@ -78,6 +151,22 @@ var view = {
       }
       if (elementClicked.nodeName === 'LI') {
         handlers.displayNote(parseInt(elementClicked.id, 10));
+      }
+    });
+    var tagsUl = document.querySelector('ul#tags');
+    tagsUl.addEventListener('click', function(event) {
+      var elementClicked = event.target;
+      if (elementClicked.className === 'tagDeleteButton') {
+        handlers.deleteTag(elementClicked.parentNode.dataset.tag);
+      }
+      if (elementClicked.nodeName === 'LI') {
+        if (elementClicked.className === 'selected') {
+          elementClicked.className = '';
+          view.displayAllNotes(elementClicked.dataset.tag);
+        } else {
+          elementClicked.className = 'selected';
+          handlers.displayNotesWithTag(elementClicked.dataset.tag);
+        }
       }
     });
   }
